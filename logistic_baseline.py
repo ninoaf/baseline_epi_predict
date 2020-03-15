c89b6aae943c63621eccdcb5ab0f27a158f78e4c
lubo93 <lucasb@ethz.ch>
2020-03-15 15:28:55 -0700

    added logistic prediction model without error bars (can be added easily from pcov values)

---
+++ logistic_baseline.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+import scipy.optimize as opt
+import numpy as np
+import datetime
+from datetime import date
+
+def logistic(x, k, x0):
+    return 1 / (1. + np.exp(-k * (x - x0)))
+
+def pred_logistic(x_train, y_train, x_pred, L):
+    # This prediction uses a logistic curve f(x)=L/(1+exp(-k*(x-x0))) as fit.
+    # L can be estimated from the herd immunity fraction of a given population
+    # In this estimate, we use a herd immunity fraction of p=1-1/R0=1-1/2.91=0.66.    
+        
+
+    x_pred = np.copy(x_pred)/max(x_train)
+
+    x_train = np.copy(x_train)/max(x_train)    
+    y_train = np.copy(y_train)/L
+    
+
+    popt, pcov = opt.curve_fit(logistic, x_train, y_train)
+    
+    print(popt, pcov)
+    
+    y_pred = logistic(x_pred, popt[0], popt[1])
+    y_pred *= L    
+    
+    return y_pred
+
+confirmed = pd.read_csv('time_series_19-covid-Confirmed.csv')
+deaths = pd.read_csv('time_series_19-covid-Deaths.csv')
+recovered = pd.read_csv('time_series_19-covid-Recovered.csv')
+
+colnames = confirmed.columns.tolist()
+
+start = datetime.datetime.strptime(colnames[4], "%m/%d/%y")
+end = datetime.datetime.strptime(colnames[-1], "%m/%d/%y")
+date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+countries = ['Italy', 'Germany']
+population = [60.5*10**6, 82.8*10**6]
+herd_imm_ratio = 0.66
+# time delta for prediction in days
+prediction_delta = 2
+
+today = date.today()
+next_pred_date = today+datetime.timedelta(days=prediction_delta)
+
+file_str = "2day_prediction_" + str(today) + ".csv"
+print(file_str)
+
+f = open(file_str,"w+")
+    #'Province/State, Country, prediction target date, N, varN, R, varR, D, varD, M, varM
+    #NaN, Switzerland, 14/03/2020, 1211, 1100-1400, 20, 10-60, 3, 3-5, 0.05, 0.01-0.1
+f.write("Province/State,Country,Prediction Target Date,N,varN,R,varR,D,varD,M,varM \n")
+
+    
+    
+for i in range(len(countries)):
+    confirmed_region = confirmed.loc[confirmed['Country/Region'] == countries[i]]
+    deaths_region = deaths.loc[deaths['Country/Region'] == countries[i]]
+    recovered_region = recovered.loc[recovered['Country/Region'] == countries[i]]
+    
+    confirmed_region = np.asarray([float(confirmed_region[colnames[4+i+1]]) for i in range(len(date_generated))])
+    deaths_region = np.asarray([float(deaths_region[colnames[4+i+1]]) for i in range(len(date_generated))])
+    recovered_region = np.asarray([float(recovered_region[colnames[4+i+1]]) for i in range(len(date_generated))])
+    days = np.linspace(0, len(date_generated)-1, len(date_generated))
+    
+    days_prediction = np.linspace(0, len(date_generated)-1+prediction_delta, len(date_generated)+prediction_delta)
+    dates_prediction = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days+prediction_delta)]
+    
+    confirmed_pred = pred_logistic(days, confirmed_region, days_prediction, population[i]*herd_imm_ratio)
+    deaths_pred = pred_logistic(days, deaths_region, days_prediction, population[i]*herd_imm_ratio)
+    recovered_pred = pred_logistic(days, recovered_region, days_prediction, population[i]*herd_imm_ratio)
+    
+    fig, ax = plt.subplots()
+    plt.title('%s'%countries[i])
+    plt.plot(date_generated, confirmed_region, 'o', label = 'confirmed cases')
+    plt.plot(date_generated, deaths_region, '^', label = 'deaths')
+    plt.plot(date_generated, recovered_region, 's', label = 'recovered')
+    plt.plot(dates_prediction, confirmed_pred, 'grey', label = 'prediction')
+    plt.plot(dates_prediction, deaths_pred, 'grey')
+    plt.plot(dates_prediction, recovered_pred, 'grey')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+    plt.xlim([min(dates_prediction), max(dates_prediction)])
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(7))
+    plt.xticks(rotation = 45)
+    plt.legend(loc = 2)
+    plt.ylabel('cases')
+    plt.tight_layout()
+    plt.show()
+    
+    next_mortality = deaths_pred/confirmed_pred
+        
+    next_pred_date_str = str(next_pred_date)+","
+    loc1_str = str(countries[i]).replace(',', ' ') + ","
+    loc2_str = "nan,"
+    n_str = str(confirmed_pred[-1])+",nan,"
+    r_str = str(deaths_pred[-1])+",nan,"
+    d_str = str(recovered_pred[-1]) + ",nan,"
+    m_str = str(next_mortality[-1])+ ",nan\n"
+    
+    f.write(loc1_str+loc2_str+next_pred_date_str+n_str+r_str+d_str+m_str)
+
+print("baseline predictions writtern to:"+file_str)    
+f.close()
+
