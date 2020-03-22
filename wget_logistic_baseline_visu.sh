68338757d40090c0c2ee7d18a6c62883c09452e7
lubo93 <lucasb@ethz.ch>
2020-03-21 18:23:34 -0700

    another wget script added

---
+++ wget_logistic_baseline_visu.sh
@@ -0,0 +1,5 @@
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+
+python3 logistic_baseline_7days_plot.py
